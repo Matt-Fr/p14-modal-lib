18a58d253cc63ffd0fed799768242fbd398aa696
Matt-Fr <matthieu_franck@orange.fr>
2022-11-07 18:13:24 +0100

    first commit

---
+++ src/lib/Modal.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import PropTypes from 'prop-types'
+import styles from './Modal.module.css'
+import { AiFillCloseCircle } from "react-icons/ai";
+
+
+export const Modal = ({ message, openModal, setOpenModal }) => {
+  return (
+    openModal && (
+      <div
+        className={styles.background}
+        onClick={() => {
+          setOpenModal(false);
+        }}
+      >
+        <div
+          className={styles.backgroundContainer}
+          onClick={() => {
+            setOpenModal(false);
+          }}
+        >
+          <h3 className={styles.backgroundContainertitle}>{message}</h3>
+          <AiFillCloseCircle className={styles.closeIcon} />
+        </div>
+      </div>
+    )
+  );
+};
